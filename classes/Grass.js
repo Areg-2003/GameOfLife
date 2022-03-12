0314dd190dedf391361f2a9aa877c7b899ccae80
user <areg.movsesyan.y@tumo.org>
2022-03-12 10:49:21 +0400

    lesson 2

---
+++ classes/Grass.js
@@ -0,0 +1,29 @@
+class Grass extends LivingCreature {
+
+    constructor(x, y, id) {
+        super(x, y);
+        this.id = id;
+    }
+
+    chooseCell(ch){
+        return super.chooseCell(ch);
+    }
+
+    mul() {
+        this.multiply++;
+        var emptyCells = this.chooseCell(0);
+        var newCell = random(emptyCells);
+
+        if (newCell && this.multiply >= 8 && this.chooseCell(5)) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = this.id;
+
+            var newGrass = new Grass(newX, newY, this.id);
+            grassArr.push(newGrass);
+            this.multiply = 0;
+        }
+    }
+
+
+}
